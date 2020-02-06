671a81779f067c9d6ec43b7579eed5427f48a89e
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-02-06 11:35:20 +0100

    TextView, EditText og Button tilføjede.

--- app/src/main/java/com/android/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/android/example/mvvmdemo/MainActivity.java
@@ -3,12 +3,24 @@ package com.android.example.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.edit_text);
+    }
+    
+    public void changeText(View view) {
+        textView.setText(editText.getText().toString());
     }
 }
